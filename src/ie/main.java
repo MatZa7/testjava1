ebd3cbcf4be1cf3f0961425af036a4f51b6fbf47
MatZa7 <C20471062@mytudublin.ie>
2022-02-07 11:59:43 +0000

    test

---
+++ src/ie/main.java
@@ -0,0 +1,12 @@
+package ie;
+public class main
+{
+    public static void main(String[] args)
+    {
+        //loops();
+        system.out.println("YO");
+        // Tara Misty
+        // Tara Tara
+        
+    }
+}
